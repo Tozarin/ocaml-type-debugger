27c7e46a7f8ea23d923c4c2433ac983c66b8469b
Startsev <tozarin@mail.ru>
2023-02-18 21:32:01 +0300

    Adds first ast
    Signed-off-by: Startsev <tozarin@mail.ru>

---
+++ lib/ast.ml
@@ -0,0 +1,57 @@
+type id = string
+type constructor_name = string
+
+type const_type = 
+  | TInt
+  | TFLoat
+  | TBool
+  | TChar
+  | TString
+  | TUnit
+  (* mb more things or another type for this*)
+
+type bin_op = 
+  | IAdd 
+  | ISub 
+  | IMul 
+  | IDiv 
+  | FAdd 
+  | FSub 
+  | FMul 
+  | FDiv 
+  | Mod 
+  | Less 
+  | Gre 
+  | Leq 
+  | Geq 
+  | Eq 
+  | Neg 
+  | And 
+  | Or 
+  | Concat
+
+type un_op = 
+  | Minus
+  | Not
+
+type recursive = 
+  | Rec
+  | Not
+
+type expr = 
+  | ELiteral of const_type
+  | EBinOp of bin_op * expr * expr
+  | EUnOp of un_op * expr
+  | EApp of expr * expr
+  | EId of id
+  | EFun of id list * expr
+  | EList of expr list
+  | ETup of expr list
+  | EDeclaration of recursive * id * id list * expr
+  | ELetIn of expr list * expr
+  | EIf of expr * expr * expr
+  | EMatch of expr * (expr * expr) list
+  (* mb type for patterns *)
+  | EConstructor of constructor_name * expr option
+
+(* TODO: custom types (single, adt, gadt mb)*)
